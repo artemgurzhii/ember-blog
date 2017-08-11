f4bed9527d819d06fe3fcc669b2d05f07e178131
Artem Gurzhii <artem.gurzhii@gmail.com>
2017-08-11 19:39:02 +0300

    removing post comments query with plain JSONAPI relationship, other minor improvements

--- app/models/post.js
+++ app/models/post.js
@@ -10,13 +10,15 @@ const {
 } = DS;
 
 const {
-  computed: {
-    notEmpty,
-    or,
-    gt,
-    equal,
-  }
+  get,
+  computed
 } = Ember;
+
+const {
+  notEmpty,
+  or,
+  gt
+} = computed;
 
 export default Model.extend(PostValidations, {
   title: attr('string'),
@@ -40,5 +42,7 @@ export default Model.extend(PostValidations, {
 
   wasEdited: gt('updated_at', 'created_at'),
 
-  isAllowed: equal('model.firstObject.user.username', 'session.currentUser.username')
+  isAllowed: computed('model.firstObject.user.username', 'session.currentUser.username', function(){
+    return get(this,'model.firstObject.user.username') === get(this,'session.currentUser.username');
+  })
 });
--- app/routes/posts/show.js
+++ app/routes/posts/show.js
@@ -2,23 +2,12 @@ import Ember from 'ember';
 
 const {
   get,
-  RSVP,
   Route
 } = Ember;
 
 export default Route.extend({
   model({ post_id: id }) {
-    const post = this.store.findRecord('post', id);
-    const comments = this.store.findAll('comment', {
-      filter: {
-        post: id
-      }
-    });
-
-    return RSVP.hash({
-      post,
-      comments
-    });
+    return this.store.findRecord('post', id);
   },
 
   titleToken(model) {
